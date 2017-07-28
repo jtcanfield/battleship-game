6fc0bc84d4421d2cc9dcbdc6b402abf3265301e0
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-28 13:55:15 -0400

    add plate randomizer

---
+++ main.js
@@ -0,0 +1,21 @@
+
+function makeNewPosition(){
+    var top = $(window).height();
+    var left = $(window).width();
+
+    var starttop = Math.floor(Math.random()*(top-0+1)+0);
+    var startleft = Math.floor(Math.random()*((left+80)-left+20)+left);
+
+    var finaltop = Math.floor(Math.random()*(top-0+1)+0);
+    var finalleft = Math.floor(Math.random()*(-80-50+1)-50);
+    var time = Math.floor(Math.random()*(4000-1000+1)+1000);
+    return [starttop,startleft,finaltop,finalleft,time];
+}
+function animateComet(name){
+    var newq = makeNewPosition();
+    $(name).animate({ top: newq[0], left: newq[1] },function(){
+    });
+    $(name).fadeIn(0);
+    $(name).animate({ top: newq[2], left: newq[3] }, newq[4], function(){
+});
+};
