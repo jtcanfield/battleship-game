23cbee89dd5040cb25778546d469135943356763
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-28 15:58:18 -0400

    add drag and drop script

--- main.js
+++ main.js
@@ -19,6 +19,21 @@ UNTIL READY
 
 */
 
+//DRAG AND DROP SCRIPT
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
 //The below must be called on an ID
 function makeNewPosition(){
     var top = $(window).height();
