dffb35ab6554537a87fa2eb1fea3c706978385ec
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-28 15:19:37 -0400

    Add animation for opponent reentry

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+$(name).hide(0);
 function main() {
 }
 $(document).ready(main);
 $(document).ready(function() {
+  animateAvatarStart("#computer_player_avatar");
 });
 
 
@@ -13,17 +15,21 @@ $(document).ready(function() {
 function makeNewPosition(){
     var top = $(window).height();
     var left = $(window).width();
+    let computer_player_box_top = (document.getElementById("computer_player_box").getBoundingClientRect()).top;
+    let computer_player_box_left = (document.getElementById("computer_player_box").getBoundingClientRect()).left;
 
     var starttop = Math.floor(Math.random()*(top-0+1)+0);
     var startleft = Math.floor(Math.random()*((left+80)-left+20)+left);
+    var finaltop = computer_player_box_top - 110;
+    var finalleft = computer_player_box_left - 110;
 
-    var finaltop = Math.floor(Math.random()*(top-0+1)+0);
-    var finalleft = Math.floor(Math.random()*(-80-50+1)-50);
     var time = Math.floor(Math.random()*(4000-1000+1)+1000);
-    return [starttop,startleft,finaltop,finalleft,time];
+    let scale = Math.floor(Math.random()*(10-5+1)+5);
+    return [starttop,startleft,finaltop,finalleft,time,scale];
 }
-function animateComet(name){
+function animateAvatarStart(name){
     var newq = makeNewPosition();
+    $(name).hide(0);
     $(name).animate({ top: newq[0], left: newq[1] },function(){
     });
     $(name).fadeIn(0);
