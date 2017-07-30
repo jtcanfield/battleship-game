abb3131c296460d9615ea7ff51bb23f06d461518
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-07-30 18:49:33 -0400

    add placing ability

--- main.js
+++ main.js
@@ -189,23 +189,33 @@ function computerPlaceShips(){
       placeBoat(1, computerShips[i], computerShipsSize[i]);
     }
     function placeBoat(v, typeOfShip, lengthOfShips){
-      let maxHorizPosition = 10-lengthOfShips;
-      let maxVertPosition = 11-lengthOfShips;
-      if (v = 0){
-        let verticalPosition = Math.floor(Math.random()*(11-2+1)+2);
-        let horizontalPosition = Math.floor(Math.random()*(maxHorizPosition-1+1)+1);
-        let verticalPosition = 1;
-        let horizontalPosition = 10;
-        $("#computer_battleship_board tr:nth-child("+horizontalPosition+") td:nth-child("+verticalPosition+")").addClass("computer_pieces");
-        // $(theTarget).attr("id", typeOfShip + "_piece"+(i+1));
-        console.log(horizontalPosition);
-        console.log(typeOfShip);
+      let maxHorizPosition = 11-lengthOfShips;
+      let maxVertPosition = 10-lengthOfShips;
+      if (v === 0){
+        //HORIZONTAL PLACING
+        let verticalPosition = Math.floor(Math.random()*(10-1+1)+1);
+        let horizontalPosition = Math.floor(Math.random()*(maxHorizPosition-2+1)+2);
+        let selectedPiece = $("#computer_battleship_board tr:nth-child("+verticalPosition+") td:nth-child("+horizontalPosition+")");
+        for (let r = 0; r < lengthOfShips; r++){
+          $(selectedPiece).addClass("computer_pieces");
+          $(selectedPiece).attr("id", typeOfShip + "_piece"+(r+1));
+          selectedPiece = $(selectedPiece).nextUntil()[0];
+        }
       } else {
-        let verticalPosition = Math.floor(Math.random()*(maxVertPosition-2+1)+2);
-        let horizontalPosition = Math.floor(Math.random()*(10-1+1)+1);
-        $("#computer_battleship_board tr:nth-child("+horizontalPosition+") td:nth-child("+verticalPosition+")").addClass("computer_pieces");
-        console.log(verticalPosition);
-        console.log(typeOfShip);
+        //VERTICAL PLACING
+        let verticalPosition = Math.floor(Math.random()*(maxVertPosition-1+1)+1);
+        let horizontalPosition = Math.floor(Math.random()*(11-2+1)+2);
+        let selectedPiece = $("#computer_battleship_board tr:nth-child("+verticalPosition+") td:nth-child("+horizontalPosition+")");
+        for (let r = 0; r < lengthOfShips; r++){
+          $(selectedPiece).addClass("computer_pieces");
+          $(selectedPiece).attr("id", typeOfShip + "_piece"+(r+1));
+          let rowIndexing = $(selectedPiece).prevUntil(".label").length;
+          let findNextRow = $(selectedPiece).parent().next();
+          if (findNextRow[0] === undefined){
+              break;
+          }
+          selectedPiece = $(findNextRow[0].childNodes[rowIndexing+1])[0];
+        }
       }
     }
   }
